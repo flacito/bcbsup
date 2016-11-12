1bebbbad2c75c82e5945ef11fc224411abd7a40f
flacito <bwebb@bbandt.com>
2016-11-12 11:19:07 -0500

    Added JSON as arg 1

--- create_machine/create_machine.go
+++ create_machine/create_machine.go
@@ -2,14 +2,26 @@ package main
 
 import (
   "fmt"
-  "github.com/flacito/gosup/messages"
+  "os"
+  "encoding/json"
 )
 
+type CreateMachineMessage struct {
+    HostName string
+    MemorySizeGB int
+    CpuCount int
+}
+
 func main() {
-  m := messages.NewMachineMessage{
-    HostName: "tapdock",
-    MemorySizeGB: 4,
-    CpuCount: 2,
+  // 1st arg is JSON, like '{"HostName":"hallo-tapco","MemorySizeGB":2,"CpuCount":2}'
+  argsWithoutProg := os.Args[1:]
+  b := []byte(argsWithoutProg[0])
+
+  var m CreateMachineMessage
+  e := json.Unmarshal(b, &m)
+  if e != nil {
+      fmt.Printf("File error: %v\n", e)
+      os.Exit(1)
   }
 
 	fmt.Printf("Processing create machine request:\n  %v\n",m.HostName)
--- messages/new_machine_message.go
+++
@@ -1,7 +0,0 @@
-package messages
-
-type NewMachineMessage struct {
-    HostName string
-    MemorySizeGB int
-    CpuCount int
-}
